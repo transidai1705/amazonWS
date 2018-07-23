a82ef135ca725530b64c89a911e35185a21992e5
Opt SD <transidai1705@gmail.com>
2018-07-23 14:33:23 +0700

    new commit

--- helloworld/lambda.js
+++ helloworld/lambda.js
@@ -1,6 +1,72 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ses = new AWS.SES();
+const ddb = new AWS.DynamoDB.DocumentClient();
+const request = require("request");
 
+exports.handler = function (event, context, callback) {
+	request.get("https://amtsblattportal.ch/",
+		(error, response, body) => {
+			if (!body) {
+				throw new Error("Failed to fetch homepage!");
+			}
 
-	callback(null,'Successfully executed');
+			let urls = page.match(/(lp|\?r=specialTopic)\/[^"]*/);
+			if (!urls) { // nothing found; no point in proceeding
+				return;
+			}
+			let url = urls[0];
+
+			ddb.get({
+				TableName: 'site-data',
+				Key: {
+					'domain': 'https://amtsblattportal.ch/'
+				}
+			}, function (err, data) {
+				if (err) {
+					throw err;
+				} else {
+					if (!data.Item || data.Item.url != url) {
+						ses.sendEmail({
+							Destination: {
+								ToAddresses: ['transidai1705@gmail.com'],
+								CcAddresses: [],
+								BccAddresses: []
+							},
+							Message: {
+								Body: {
+									Text: {
+										Data: url
+									}
+								},
+								Subject: {
+									Data: 'MyFavSite Update!'
+								}
+							},
+							Source: 'transidai1705@gmail.com'
+						}, function (err, data) {
+							if (err) {
+								throw err;
+							}
+							ddb.put({
+								TableName: 'site-data',
+								Item: {
+									'domain': 'https://amtsblattportal.ch/',
+									'url': url
+								}
+							}, function (err, data) {
+								if (err) {
+									throw err;
+								} else {
+									console.log("New URL saved successfully!");
+								}
+							});
+						});
+					} else {
+						console.log("URL already sent out; ignoring");
+					}
+				}
+			});
+		});
+
+	callback(null, 'Successfully executed');
 }
